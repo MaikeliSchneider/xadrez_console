e13ddea9ee6f6100aabf5ee09c657ab29ad0b6ad
Maikeli Schneider <maikeli@logcte.com.br>
2024-12-12 10:30:59 -0300

    Criando a classe posicao xadrez

--- xadrez_console/Program.cs
+++ xadrez_console/Program.cs
@@ -5,20 +5,24 @@ namespace xadrez {
     internal class Program {
         static void Main(string[] args) {
 
-            try {
-                Tabuleiro tab = new Tabuleiro(8,8);
-                tab.colocarPeca(new Torre(tab,Cor.Preta),new Posicao(0,0));
-                tab.colocarPeca(new Torre(tab,Cor.Preta),new Posicao(1,3));
-                tab.colocarPeca(new Rei(tab,Cor.Preta),new Posicao(0,2));
+            PosicaoXadrez pos = new PosicaoXadrez('c',7);
+            Console.WriteLine(pos);
+            Console.WriteLine(pos.toPosicao());
+            Console.ReadLine();
+            /* try {
+                 Tabuleiro tab = new Tabuleiro(8,8);
+                 tab.colocarPeca(new Torre(tab,Cor.Preta),new Posicao(0,0));
+                 tab.colocarPeca(new Torre(tab,Cor.Preta),new Posicao(1,3));
+                 tab.colocarPeca(new Rei(tab,Cor.Preta),new Posicao(0,2));
 
-                Tela.imprimirTabuleiro(tab);
-            }
+                 Tela.imprimirTabuleiro(tab);
+             }
 
-            catch (TabuleiroException e) {
-                Console.WriteLine(e.Message);
+             catch (TabuleiroException e) {
+                 Console.WriteLine(e.Message);
 
-                Console.ReadLine();
-            }
+                 Console.ReadLine();
+             }*/
         }
     }
 }
---
+++ xadrez_console/xadrez/PosicaoXadrez.cs
@@ -0,0 +1,22 @@
+﻿using tabuleiro;
+
+namespace xadrez {
+    internal class PosicaoXadrez {
+
+        public char colunas { get; set; }
+        public int linha { get; set; }
+
+        public PosicaoXadrez(char colunas, int linha) {
+            this.colunas = colunas;
+            this.linha = linha;
+        }
+
+        public Posicao toPosicao() {
+            return new Posicao(8 - linha, colunas - 'a');
+        }
+        public override string ToString() {
+            return $" {colunas} {linha}";
+
+        }
+    }
+}
