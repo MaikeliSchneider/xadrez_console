b6da11ef7c55c2d25aa3d42ba064cd232811ebae
Maikeli Schneider <sfsmaikeli@gmail.com>
2024-12-13 09:34:39 -0300

    Controlando pecas capturadas

--- xadrez_console/Program.cs
+++ xadrez_console/Program.cs
@@ -11,7 +11,7 @@ namespace xadrez {
 
                     try {
                         Console.Clear();
-                        Tela.imprimirTabuleiro(partida.tab);
+                        Tela.imprimirPartida(partida);
 
                         Console.WriteLine();
                         Console.WriteLine($"Turno: {partida.turno}");
